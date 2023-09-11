a52147373a5ee99958c0e7531ed517098d600ac1
sou23ad <elhintesouad@gmail.com>
2023-09-10 18:38:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
 /**
  * main - entry poit
  * return: always 0 (success)
-*/int main(void)
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
         return (0);
